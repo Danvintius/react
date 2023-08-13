7253ff281b9fa246ff98e32441452072b96c56cd
Danvintius <danila.94@mail.ru>
2023-08-13 16:45:40 +0300

    props

--- src/components/ShopItemFunc.js
+++ src/components/ShopItemFunc.js
@@ -1,13 +1,5 @@
 import './ShopItemFunc.css';
-
-const item = {
-    brand: 'Tiger of Sweden',
-    title: 'Leonard coat',
-    description: 'Minimalistic coat in cotton-blend',
-    descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
-    price: 399,
-    currency: '£'
-  }
+import {item} from App.js;
   
 function ShopItemFunc(props) {
     const {item} = props;
